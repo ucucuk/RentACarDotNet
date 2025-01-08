2148868d19ba9cc8b0981ec8941a9c33592184f2
Ulaş Cücük <ulascucuk@gmail.com>
2025-01-08 13:11:29 +0300

    UIVue

---
+++ UIVue/wwwroot/js/rentacar.js
@@ -0,0 +1,15 @@
+﻿
+var app = new Vue({
+	el: "#app1",
+	data: {
+		baslik: "Kisiler",
+		gorunsun: true,
+		olussun: true,
+		isimler: [{ isim: "Ulas" }, { isim: "Cucuk" }]
+	},
+	methods: {
+		denemeClick: function () {
+			this.gorunsun = !this.gorunsun;
+		}
+	}
+});
